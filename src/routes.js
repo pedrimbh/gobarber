fee5dc787b73fb8bc39bff7afc29b2222469c425
pedro.mesquita <joaopmesquita@gmail.com>
2019-09-29 19:17:38 -0500

    App bastante avancada, esqueci de push por que estava com muito trabalho

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionContrroller';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
-
+const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
